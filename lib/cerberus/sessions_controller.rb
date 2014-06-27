c81042c77085585e487d0032a2a8eb0117b2ad2c
Nicolas Oga <2112.oga@gmail.com>
2014-06-26 21:12:51 -0300

    FTC

--- lib/cerberus/sessions_controller.rb
+++ lib/cerberus/sessions_controller.rb
@@ -3,7 +3,7 @@ require 'cerberus/identity'
 class Cerberus::SessionsController < ActionController::Base
   def create
     identity = Cerberus::Identity.find_or_create_by! auth.slice(:provider, :uid)
-    identity.user ||= Cerberus.config.user_class.constantize.find_or_create_by! auth.slice(:email)
+    identity.user ||= Cerberus.config[:identities][:user_class_name].constantize.find_or_create_by! auth.slice(:email)
     identity.save
 
     session[:user_id] = identity.user.id
