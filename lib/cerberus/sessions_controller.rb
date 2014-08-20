b7530d6e74d68057fab49319ebc0b84d8ce50aba
Nicolas Oga <2112.oga@gmail.com>
2014-08-19 20:08:50 -0500

    Use strong parameters

--- lib/cerberus/sessions_controller.rb
+++ lib/cerberus/sessions_controller.rb
@@ -2,7 +2,7 @@ require 'cerberus/identity'
 
 class Cerberus::SessionsController < ActionController::Base
   def create
-    identity = Cerberus::Identity.find_or_create_by! auth.slice(:provider, :uid)
+    identity = Cerberus::Identity.find_or_create_by! identity_params
     identity.user ||= Cerberus.config[:identities][:user_class_name].constantize.find_or_create_by! email: auth[:info][:email]
     identity.save
 
@@ -18,4 +18,7 @@ class Cerberus::SessionsController < ActionController::Base
   private
 
   def auth() env['omniauth.auth'] end
+  def auth_params() @auth_params ||= ActionController::Parameters.new(auth) end
+  def identity_params() auth_params.permit(:provider, :uid) end
+  def user_params() auth_params.permit(info: [:email]) end
 end
